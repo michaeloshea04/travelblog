32d62244358af0e578e02ecf4c4e25824edba1b7
michaeloshea04 <michael.oshea1@jcu.edu.au>
2023-04-28 11:46:07 +0100

    add more location pages

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 // Get the modal
 var modal = document.getElementsByClassName("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementsByClassName("myBtn");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementsByClassName("img01");
+var captionText = document.getElementsByClassName("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
